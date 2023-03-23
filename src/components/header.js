e101073c55e2616431aee0649112fc3e716a59b1
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-03-23 13:53:26 +0100

    header

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Filmland from "../assets/images/Logo.svg";
+import menu from "../assets/images/menu.svg";
+import search from "../assets/images/search.svg"
+
+const Header = () => {
+  return (
+    <div className="header_container">
+      <div className="logo_container">
+        <img src={Filmland} alt="filalndlogo" />
+      </div>
+      <div className="box">
+        <input type="text" placeholder="What do you want to watch?" name="search" />
+        <img src={search} alt="searchicon" />
+      </div>
+      <div className="menu_container">
+        <p>Hi, $Fullname</p>
+        <img src={menu} alt="menu" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
