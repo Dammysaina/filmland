da385f42dfa9fd97f3420eea70c9f6c9cf386d53
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-03-24 01:46:19 +0100

    card

--- src/components/card.js
+++ src/components/card.js
@@ -3,13 +3,26 @@ import React from "react";
 const Card = () => {
     return (
         <div>
-            {/* <div>
+            <div>
                 <img src={props.image} alt="service"></img>
+                <p></p>
+                <img></img>
             </div>
             <div className="selection_text">
                 <h1>{props.header}</h1>
                 <p>{props.body}</p>
-            </div> */}
+                <div>
+                    <div>
+                        <img></img>
+                        <p></p>
+                    </div>
+                    <div>
+                        <img></img>
+                        <p></p>
+                    </div>
+                </div>
+                <p></p>
+            </div>
         </div>
     )
 }
---
+++ src/components/selection.js
@@ -0,0 +1,20 @@
+import vessel2 from "../assets/images/vessel2.jpeg";
+import vessels5 from "../assets/images/vessels5.jpeg"
+
+const Selection = [
+    {
+        id:1,
+        image: vessel2,
+        header: "BERTHING AND MOORING",
+        body: "Consequat Lorem tempor Lorem officia reprehenderit laboris incididunt eiusmod id Lorem nostrud."
+    },
+    {
+        id:2,
+        image: vessels5,
+        header: "MARINE SERVICES",
+        body: "Magna quis laborum nulla incididunt cillum laboris deserunt aliqua non proident. Sunt et ut et aliqua consequat nulla fugiat mollit est irure nisi deserunt esse esse. Dolore magna quis sunt ea non Lorem. Incididunt velit voluptate do proident et proident nostrud sit voluptate. Eu qui non qui quis adipisicing non non eiusmod fugiat exercitation officia do consequat reprehenderit. Eu id aliqua magna enim duis ad nisi aliquip. Esse anim minim est tempor ea eu sunt do nostrud aliqua proident ex minim."
+    },
+    
+]
+
+export default Selection;
