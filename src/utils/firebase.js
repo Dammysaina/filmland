48a43ccf84022152b6eb07e687bf9e1f6f6e7b20
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-03-26 18:27:32 +0100

    gitignore

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDbQSd9g-tKSVksNzQB77RptmDpXq6lmAM",
-  authDomain: "filmland-f3c60.firebaseapp.com",
-  projectId: "filmland-f3c60",
-  storageBucket: "filmland-f3c60.appspot.com",
-  messagingSenderId: "269048560105",
-  appId: "1:269048560105:web:1ffc88c8e028fe0b557378",
-  measurementId: "G-MB0500Q3BN"
+  apiKey: "process.env.AIzaSyDbQSd9g-tKSVksNzQB77RptmDpXq6lmAM",
+  authDomain: "process.env.filmland-f3c60.firebaseapp.com",
+  projectId: "process.env.filmland-f3c60",
+  storageBucket: "process.env.filmland-f3c60.appspot.com",
+  messagingSenderId: "process.env.269048560105",
+  appId: "process.env.1:269048560105:web:1ffc88c8e028fe0b557378",
+  measurementId: "process.env.G-MB0500Q3BN"
 };
 
 // Initialize Firebase
